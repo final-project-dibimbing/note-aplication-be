18eb1394e221c3325abd39646ac6cd939d64bae6
dienul <44157207+dienul@users.noreply.github.com>
2022-01-12 22:46:31 +0700

    create service, module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,14 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './config/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { NotebooksModule } from './notebooks/notebooks.module';
+import { NotesModule } from './notes/notes.module';
+import { TagsModule } from './tags/tags.module';
+import { ShareModule } from './share/share.module';
 
 
 @Module({
-  imports: [UsersModule, DatabaseModule, AuthModule],
+  imports: [UsersModule, DatabaseModule, AuthModule, NotebooksModule, NotesModule, TagsModule, ShareModule],
   controllers: [AppController],
   providers: [AppService],
 })
