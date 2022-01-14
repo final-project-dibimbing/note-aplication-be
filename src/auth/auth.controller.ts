fe24f63b2eae79a3f9bf428c801ab3c6e9ff1eef
dienul <44157207+dienul@users.noreply.github.com>
2022-01-15 00:46:59 +0700

    update service in auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseInterceptors , Query, Get,Headers, UseGuards} from "@nestjs/common";
+import { ApiBearerAuth, ApiProperty, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { SignUpReq } from './request/sign-up.request';
+import { RespSuccessInteceptor } from "../middleware/interceptor/resp-success.inteceptor";
+import { SignInRequest } from "./request/sign-in.request";
+import { LoginInteceptor } from "../middleware/interceptor/login.inteceptor";
+import { MustGuard } from "src/middleware/guard/must.guard";
 
+@UseInterceptors(RespSuccessInteceptor)
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
@@ -13,8 +18,17 @@ export class AuthController {
     return await this.service.signUp(data);
   }
 
+  @UseInterceptors(LoginInteceptor)
   @Post('sign-in')
-  async signIn(@Body() data : any){
-    return await this.service.signIn(data);
+  async signIn(@Body() data : SignInRequest, @Query() query:any){
+    return await this.service.signIn(query.payload);
+  }
+
+  @UseGuards(MustGuard)
+  @ApiBearerAuth("token")
+  @Get('me')
+  async me(@Headers() header:any){
+    console.log("header > ", header.user.id)
+    return await this.service.me(header.user.id)
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, Inject } from '@nestjs/common';
 import { UserEntity } from 'src/entity/users.entity';
 import { Repository } from 'typeorm';
 import { SignUpReq } from './request/sign-up.request';
+import { SignInRequest } from './request/sign-in.request';
+
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
@@ -19,29 +21,23 @@ export class AuthService {
 
     const saveUser = await this.userRepository.save(data);
     console.log('saveUser >', saveUser);
-    return 'OK BERHASIL';
+    return 'Sign up success';
   }
 
-  public async signIn(data: any) {
-    const user = await this.userRepository.findOne({
-      where: {
-        email: data.email,
-      },
-    });
-
-    if (!user) throw new Error('User not found');
-    if (!(await this.compare(data.password, user.password)))
-      throw new Error('Invalid email or password');
-
-    return await jwt.sign(
-      {
-        id: user.id,
-        username: user.fullname,
-        address: user.email,
-        phone_number: user.phone_number,
-      },
-      'SECRET', //process.env.SECRET
-    );
+  public async signIn(user: any) {
+    return {
+      fullname: user.fullname,
+      email: user.email,
+      token: await jwt.sign(
+        {
+          id: user.id,
+          username: user.fullname,
+          address: user.email,
+          phone_number: user.phone_number,
+        },
+        'SECRET', //process.env.SECRET
+      ),
+    };
   }
 
   private async hash(password: string) {
@@ -50,7 +46,12 @@ export class AuthService {
     return hash;
   }
 
-  private async compare(s: string, hash: string) {
-    return await bcrypt.compareSync(s.toString(), hash);
+  public async me(id: any) {
+    return await this.userRepository.findOne({ 
+      select : ['id', 'fullname', 'email', 'phone_number', 'picture'],
+      where: {
+        id
+      },
+    });
   }
 }
