5e064dce934819cb569f2fa14bc944dcc820dd59
dienul <44157207+dienul@users.noreply.github.com>
2022-01-30 12:09:47 +0700

    initial commit

--- src/notes/request/index.request.ts
+++ src/notes/request/index.request.ts
@@ -1,15 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsOptional, IsString } from 'class-validator';
+import { IsArray, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class AddNoteRequest {
   @ApiProperty()
   notebook_id: number;
-@IsString()
+
+  @IsString()
   @ApiProperty()
   title: string;
+
   @IsString()
   @ApiProperty()
   content: string;
+
   @IsOptional()
   @IsArray()
   @ApiProperty()
@@ -17,39 +20,43 @@ export class AddNoteRequest {
 }
 
 export class UpdateNoteRequest {
+  @IsNumber()
   @ApiProperty()
   id: number;
 
+  @IsString()
   @ApiProperty()
   title: string;
 
+  @IsString()
   @ApiProperty()
   content: string;
 
+  @IsOptional()
+  @IsArray()
   @ApiProperty()
   tags: number[];
 }
 
-type Sort  = "ASC" | "DESC";
-export class listSort{
-  @ApiProperty({examples : ["ASC", "DESC"]})
+type Sort = 'ASC' | 'DESC';
+export class listSort {
+  @ApiProperty({ examples: ['ASC', 'DESC'] })
   update_at: string;
 
-  @ApiProperty({examples : ["ASC", "DESC"]})
+  @ApiProperty({ examples: ['ASC', 'DESC'] })
   create_at: string;
 
-  @ApiProperty({examples : ["ASC", "DESC"]})
-  title : string
+  @ApiProperty({ examples: ['ASC', 'DESC'] })
+  title: string;
 }
-
 
 const ExampleSearchNote = {
-  title : "string",
-  tags : ["string", "string"],
-  notebook_id : "string",
-  create_at : "string",
-  update_at : "string",
-}
+  title: 'string',
+  tags: ['string', 'string'],
+  notebook_id: 'string',
+  create_at: 'string',
+  update_at: 'string',
+};
 export class SearchNoteRequest {
   @ApiProperty()
   title: string;
